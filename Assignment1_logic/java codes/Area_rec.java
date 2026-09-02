d05b7a89a8f95e018e9b1e4b66f46d1ee2eca965
piyushsahanecmaug26 <piyush.sahane.cmaug26@gmail.com>
2026-09-02 09:39:49 +0530

    Delete Assignment1_logic directory

--- Assignment1_logic/java codes/Area_rec.java
+++
@@ -1,8 +0,0 @@
-class Arearec{
-	public static void main (String args[]){
-	int length = 4;
-	int width = 5;
-	int area = length*width;
-	System.out.println(area);
-  }
-}
--- Assignment1_logic/java codes/Area_square.java
+++
@@ -1,7 +0,0 @@
-class Area{
-	public static void main (String args[]){
-	int side = 4;
-	int area = side*side;
-	System.out.println(area);
-  }
-}
