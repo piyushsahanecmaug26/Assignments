84c6731892822382d005eb4f995f2aae9a42910c
piyushsahanecmaug26 <piyush.sahane.cmaug26@gmail.com>
2026-09-02 09:39:59 +0530

    Delete Assignment 4 directory

--- Assignment 4/CompareString.java
+++
@@ -1,9 +0,0 @@
-class CompareString{
-	public static void main(String[] args){
-		String str1 = "Hello";
-		String str2 = "Hello";
-		System.out.println("Both variables point to the same object:" + str1 == str2);
-
-	}
-
-}
